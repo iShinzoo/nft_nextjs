887b7b4030c4f8d374e7053c90e6b2c5b7c351ab
krishna thakur <115836360+krsnaimp@users.noreply.github.com>
2024-12-25 12:04:52 +0530

    minted nft using wagmi #4

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import "@/styles/globals.css";
+import { WagmiConfig, createClient } from "wagmi";
+import { getDefaultProvider } from "ethers";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const client = createClient({
+	autoConnect: true,
+	provider: getDefaultProvider(),
+});
+
+function MyApp({ Component, pageProps }) {
+	return (
+    <WagmiConfig client={client}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  )
 }
+
+export default MyApp;
+
--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,45 @@
 import Head from "next/head";
-import { useState } from "react";
+import { use, useState } from "react";
 import { CONTRACT_ADDRESS, CONTRACT_ABI } from "@/utils/contract";
-import { ethers } from "ethers";
+import { useAccount, useConnect, useContract, useSigner, chain } from "wagmi";
+import { InjectedConnector } from "@wagmi/core/connectors/injected";
+import { sepolia } from "wagmi/chains";
 
 export default function Home() {
-  const [address, setAddress] = useState(null);
-  const [Contract, setContract] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [hydrated, setHydrated] = useState(false);
 
-  const connectWallet = async () => {
-    if (typeof window !== "undefined") {
-      const { ethereum } = window;
-      if (ethereum) {
-        try {
-          const accounts = await ethereum.request({
-            method: "eth_requestAccounts",
-          });
-          console.log(accounts);
-          setAddress(accounts[0]);
+  const { address } = useAccount();
+  const { connect } = useConnect({
+    connector: new InjectedConnector({
+      chains: [sepolia],
+    }),
+  });
 
-          const provider = new ethers.providers.Web3Provider(ethereum);
-          const contractInstance = new ethers.Contract(
-            CONTRACT_ABI,
-            CONTRACT_ADDRESS,
-            signer
-          );
-          setContract(contractInstance);
-        } catch (error) {
-          console.error("Error connecting wallet:", error);
-        }
-      } else {
-        console.error("Ethereum wallet not found!");
-      }
-    }
-  };
+  const { data: signer } = useSigner({
+    chainId: sepolia.id,
+  });
 
-  console.log("Address ", address)
-  console.log("CONTRACT ", Contract)
-  console.log("LOADING ", loading)
+  console.log("Signer ", signer);
+  const contract = useContract({
+    address: CONTRACT_ADDRESS,
+    abi: CONTRACT_ABI,
+    signerOrProvider: signer,
+  });
+
+  use(() => {
+    setHydrated(true);
+  }, []);
+
+  console.log("Address ", address);
+  console.log("CONTRACT ", contract);
 
   const mintNFT = async () => {
     try {
-      setLoading(true);
       const mint = await contract.safeMint();
       await mint.wait();
       console.log("Minted NFT : ", mint);
-      setLoading(false);
     } catch (error) {
       console.log("Error minting NFT: ", error);
-      setLoading(false);
     }
   };
 
@@ -61,23 +52,17 @@ export default function Home() {
       </Head>
       <main className="flex items-center justify-center min-h-screen">
         <h1 className="text-4xl font-extrabold">Interact with Contract</h1>
-        {loading ? (
-          <h1 className="text-2xl font-extrabold">Loading...</h1>
-        ) : address ? (
-          <button
-            onClick={mintNFT}
-            className="py-2 px-4 rounded-xl bg-black text-white transform hover:scale-105"
-          >
-            Mint NFT
-          </button>
+        {
+         hydrated ? (
+          address ? (
+            <button onClick={mintNFT} className='py-2 px-4 rounded-xl bg-black text-white transform hover:scale-105'>Mint NFT</button>
+            ) : (
+            <button onClick={() => connect()} className='py-2 px-4 rounded-xl bg-black text-white transform hover:scale-105'>{address ? 'Mint' : 'Connect Wallet'}</button>
+          )
         ) : (
-          <button
-            onClick={connectWallet}
-            className="py-2 px-4 rounded-xl bg-black text-white transform hover:scale-105"
-          >
-            Connect Wallet
-          </button>
-        )}
+          null
+        )
+        }
       </main>
     </div>
   );
